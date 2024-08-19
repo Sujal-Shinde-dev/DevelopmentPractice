f12492cb7998f5118e3576f7c0abd759428a53c8
Sujal-Shinde <sujalshinde1404@gmail.com>
2024-08-19 17:54:01 +0530

    Some more jwt and models practice

---
+++ src/models/video.model.js
@@ -0,0 +1,38 @@
+import mongoose,{Schema} from mongoose
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+const videoSchema=new Schema({
+    duration:{
+        type:Number,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    ownedBy:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    },
+    videoFile:{
+        type:String
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    views:{
+        type:Number,
+        required:true,
+        default:0
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+    },
+    thumbnail:{
+        type:String,
+        required:true
+    }
+},{timestamps:true})
+videoSchema.plugin(mongooseAggregatePaginate)
+export const Video=new MongooseError.model("Video",videoSchema)
