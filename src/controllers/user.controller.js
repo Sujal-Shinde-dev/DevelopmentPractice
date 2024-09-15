3d0cc9ff07882eeb388f5eb67a92ea6d64eab796
Sujal-Shinde <sujalshinde1404@gmail.com>
2024-09-15 11:04:50 +0530

    bugs fixed in user controller and cloudinary

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import {ApiError} from "../utils/ApiError.js"
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import { fileUpload } from "../utils/cloudinary.js";
-import {ApiResponse} from "../utils/ApiResponse.js"
+import {ApiResponse} from "../utils/ApiResponse.js";
 const registerUser=asyncHandler(async(req ,res)=>{
   const {fullname,email,username,password}=req.body
   if(
@@ -11,7 +11,7 @@ const registerUser=asyncHandler(async(req ,res)=>{
   {
     throw new ApiError(400,"All fields are required")
 }
-    const existedUser=User.findOne({
+    const existedUser=await User.findOne({
         $or:[{username},{email}]
     })
     if(existedUser)
@@ -19,13 +19,22 @@ const registerUser=asyncHandler(async(req ,res)=>{
         throw new ApiError(409,"User with email or username already exists")
     }
     const avatarLocalPath=req.files?.avatar[0]?.path
-    const coverImageLocalPath=req.files?.coverImage[0]?.path
+    //const coverImageLocalPath=req.files?.coverImage[0]?.path
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0)
+    {
+        coverImageLocalPath=req.files.coverImage[0].path
+    }
     if(!avatarLocalPath)
     {
         throw new ApiError(400,"Avatar is required")
     }
+    //console.log("Coverimage",coverImageLocalPath)
+    //console.log("Avatarlocalpath",avatarLocalPath)
+    //console.log(req.files)
     const avatar=await fileUpload(avatarLocalPath)
     const coverImage=await fileUpload(coverImageLocalPath)
+    //console.log(req.files)
     if(!avatar)
     {
         new ApiError(400,"Avatar is Required")
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,17 +5,19 @@ cloudinary.config({
     api_key:process.env.API_KEY,
     api_secret:process.env.API_SECRET // Click 'View API Keys' above to copy your API secret
 });
+
+
 const fileUpload=async (localfilepath)=>{
     try {
         if(!localfilepath) return null
-        const response=await cloudinary.upload.uploader(localfilepath,{
+        const response=await cloudinary.uploader.upload(localfilepath,{
             resource_type:'auto'
-        })
+        });
         console.log("File upload on cloudinary url",response.url)
-
+        fs.unlinkSync(localfilepath)
         return response
     } catch (error) {
-        fs.unlinkSync(localfilepath)
+        console.log("File upload error",error);//Added
         return null
     }
 }
