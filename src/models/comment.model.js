05f4aafa7ffc8169a2a3b365a3dc87f93d73f4e2
Sujal-Shinde <sujalshinde1404@gmail.com>
2024-09-27 14:50:29 +0530

    Comment added for paginate

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,5 +1,5 @@
 import mongoose,{Schema} from "mongoose";
-import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";//If more comments are their so to go to next page
 const commentSchema=new Schema({
     video:{
         type:Schema.Types.ObjectId,
